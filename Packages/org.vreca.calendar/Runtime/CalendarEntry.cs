cc6d23140cb784287f1214fc0ec916f7cab7e036
JLChnToZ <jeremylam619@gmail.com>
2026-04-21 00:08:27 +0800

    CalendarV2

---
+++ Packages/org.vreca.calendar/Runtime/CalendarEntry.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using VRC.SDK3.Data;
+using VRC.Economy;
+using UdonSharp;
+using JLChnToZ.VRC.Foundation;
+
+namespace VRCEA.Calendar {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class CalendarEntry : UdonSharpBehaviour {
+        [SerializeField] TMP_Text dateText, titleText;
+        [SerializeField, Multiline] string dateFormat, titleFormat;
+        [SerializeField, BindEvent(nameof(Button.onClick), nameof(_ButtonClick))] Button clickButton;
+        StringBuilder sb;
+#if COMPILER_UDONSHARP
+        public
+#else
+        internal
+#endif
+        DataDictionary data;
+        string title, summary, groupId, tags;
+        DateTime timeStart, timeEnd;
+        double duration;
+
+#if COMPILER_UDONSHARP
+        public
+#else
+        internal
+#endif
+        void _onVarChange_data() {
+            ParseString("title", out title);
+            ParseString("summary", out summary);
+            ParseString("group_id", out groupId);
+            ParseTime("time_start", out timeStart);
+            ParseTime("time_end", out timeEnd);
+            ParseStringArray("tags", " ", "#{0}", out tags);
+            duration = (timeEnd - timeStart).TotalHours;
+            dateText.text = string.Format(dateFormat, title, summary, groupId, tags, timeStart, timeEnd, duration, "", "");
+            titleText.text = string.Format(titleFormat, title, summary, groupId, tags, timeStart, timeEnd, duration, "", "");
+            gameObject.SetActive(true);
+        }
+
+        void ParseString(string key, out string value) {
+            if (data.TryGetValue(key, TokenType.String, out var dt)) {
+                value = dt.String;
+                return;
+            }
+            value = "";
+        }
+
+        void ParseTime(string key, out DateTime time) {
+            if (data.TryGetValue(key, TokenType.String, out var dt) &&
+                DateTime.TryParse(dt.String, out var t)) {
+                time = t.ToLocalTime();
+                return;
+            }
+            time = default;
+        }
+
+        void ParseStringArray(string key, string separator, string format, out string values) {
+            if (data.TryGetValue(key, TokenType.DataList, out var dt)) {
+                var list = dt.DataList;
+                if (sb == null) sb = new StringBuilder();
+                else sb.Clear();
+                for (int i = 0, count = list.Count; i < count; i++) {
+                    dt = list[i];
+                    sb.AppendFormat(format, dt);
+                    if (i < count - 1) sb.Append(separator);
+                }
+                values = sb.ToString();
+                return;
+            }
+            values = "";
+        }
+
+#if COMPILER_UDONSHARP
+        public
+#else
+        internal
+#endif
+        void _ButtonClick() {
+            if (string.IsNullOrEmpty(groupId)) return;
+#if DEBUG
+            Debug.Log($"Opening group page: {groupId}");
+#endif
+            Store.OpenGroupPage(groupId);
+        }
+    }
+}
---
+++ Packages/org.vreca.calendar/Runtime/CalendarLoaderV2.cs
@@ -0,0 +1,51 @@
+﻿
+using JLChnToZ.VRC.Foundation;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Data;
+using VRC.SDK3.Image;
+using VRC.SDK3.StringLoading;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+namespace VRCEA.Calendar {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class CalendarLoaderV2 : UdonSharpBehaviour {
+        [SerializeField] VRCUrl dataUrl;
+        [SerializeField] VRCUrl[] imageUrls;
+        [SerializeField] GameObject entryPrefab;
+        Transform entryParent;
+        DataList spawnedEntries;
+
+        void Start() {
+            _Reload();
+            entryParent = entryPrefab.transform.parent;
+        }
+
+        public void _Reload() {
+            VRCStringDownloader.LoadUrl(dataUrl, (IUdonEventReceiver)(object)this);
+        }
+
+        public override void OnStringLoadSuccess(IVRCStringDownload result) {
+            if (!VRCJson.TryDeserializeFromJson(result.Result, out var data) || data.TokenType != TokenType.DataList) return;
+            var rawData = data.DataList;
+            if (spawnedEntries == null) spawnedEntries = new DataList();
+            int count = rawData.Count;
+            int spawnedCount = spawnedEntries.Count;
+            for (int i = 0; i < count; i++) {
+                CalendarEntry entryHandler;
+                if (i >= spawnedCount) {
+                    var entry = Instantiate(entryPrefab);
+                    entry.transform.SetParent(entryParent, false);
+                    entryHandler = entry.GetComponent<CalendarEntry>();
+                    spawnedEntries.Add(entryHandler);
+                } else
+                    entryHandler = (CalendarEntry)spawnedEntries[i].Reference;
+                entryHandler.data = rawData[i].DataDictionary;
+            }
+            for (int i = count; i < spawnedCount; i++)
+                ((CalendarEntry)spawnedEntries[i].Reference).gameObject.SetActive(false);
+        }
+    }
+}
